16a536cc800944c0fadd5ec1c14b2be82a8902f3
hemanth1313 <hemanthj03@gmail.com>
2022-07-21 18:07:04 +0530

    Compound Intrest

--- Assignment/A5.py
+++ Assignment/A5.py
@@ -3,4 +3,4 @@ t = int(input("Enter the time in years  :"))
 r = int(input("Enter the rate of intrest :"))
 n = int(input("Entre the n value :"))
 CI=p*(1+(r/n)**n*t)-p
-print("Compound Intrest =",round(CI,2))
+print("Compound Intrest =",round(CI,3))
