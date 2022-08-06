e99549f2c95824e3b4016e0cb4ea92fe89b719e8
hemanth1313 <hemanthj03@gmail.com>
2022-08-06 11:45:35 +0530

    Merge Dictionary

--- Dictionaries.py
+++ Dictionaries.py
@@ -7,4 +7,4 @@ print(Dict)
 # Creating a dictionary With integer keys
 Dict1 = {1: "Dell", 2: "Hp laptops", 3:"Acer",4:"MSI"}
 Dict2 = {"First":"Second","Third":"world", 1234:[1,2,'Hello world']}
-print(Dict1[4])
+print(Dict1)
